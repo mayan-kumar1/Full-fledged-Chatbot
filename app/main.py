4fc195d80f3b880eac17adc3991df5126206e9fc
Mayan Kumar <mayankumar62926@outlook.com>
2026-02-19 13:50:47 +0530

    new secret key

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.api.api_v1.api import api_router
 from contextlib import asynccontextmanager
 from app.services import database
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -13,4 +14,13 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(title="Chat with PDFs", lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(api_router, prefix="/api/v1")
